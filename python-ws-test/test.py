cc9525f046e491967af70d1d8a5a0561ae35060a
Pedro Henrique Leonardi <pedrleonardi@gmail.com>
2020-10-22 15:44:56 -0300

    Teste 2 Variaveis Ambiente Teste

--- python-ws-test/test.py
+++ python-ws-test/test.py
@@ -2,6 +2,7 @@ import os
 
 LOGIN_EMAIL = os.environ.get('USR_MAIL' )
 SENHA_EMAIL = os.environ.get('PAS_MAIL')
+LINGUAGENS = os.environ.get('LANG')
 
 print("Olá Mundo")
 print(LOGIN_EMAIL, SENHA_EMAIL)
