aaec15d0081313126487f5994b8d1036e990d996
Pedro Henrique Leonardi <pedrleonardi@gmail.com>
2020-10-22 15:21:59 -0300

    Correcao 2 Variaveis Ambiente Teste

--- python-ws-test/test.py
+++ python-ws-test/test.py
@@ -1,7 +1,7 @@
 import os
 
-LOGIN_EMAIL = "USR_MAIL" 
-SENHA_EMAIL = "PAS_MAIL"
+LOGIN_EMAIL = os.environ.get("USR_MAIL" )
+SENHA_EMAIL = os.environ.get("PAS_MAIL")
 
 print("Olá Mundo")
 print(LOGIN_EMAIL, SENHA_EMAIL)
