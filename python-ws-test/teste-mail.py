39715847a902ea0868502c3c92b9e61b980c754c
Pedro Henrique Leonardi <pedrleonardi@gmail.com>
2020-10-23 09:37:34 -0300

    Escondendo email

--- python-ws-test/teste-mail.py
+++ python-ws-test/teste-mail.py
@@ -13,7 +13,7 @@ time.sleep(3.5)
 msg = EmailMessage()
 msg['Subject'] = 'Email com variáveis de ambiente ocultas'
 msg['From'] = ENDERECO_EMAIL
-msg['To'] = 'pedrleonardi@gmail.com'
+msg['To'] = ENDERECO_EMAIL
 msg.set_content('Este email foi enviado com variáveis de ambiente ocultas e fornecidas pelo próprio jenkins\n' +
 'Se conseguirmos ver essa mensagem, o teste foi um sucesso :)')
 
