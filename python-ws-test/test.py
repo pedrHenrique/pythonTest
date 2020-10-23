7c36cacb700a1bdf7360dd624f3206ba2a97f1a9
Pedro Henrique Leonardi <pedrleonardi@gmail.com>
2020-10-23 14:37:06 -0300

    TestLists

--- python-ws-test/test.py
+++ python-ws-test/test.py
@@ -2,9 +2,12 @@ import os
 
 def main():
     LINGUAGENS = os.environ.get('LANG')
+    CONTATOS = [os.environ.get('CONTACTS')]
 
     print("Olá Mundo")
     print("As linguagens que estão sendo procuradas são:", LINGUAGENS)
+    print("Os contatos quais a mensagem será enviada são: ", CONTATOS)
+    print(type(CONTATOS))
 
     return LINGUAGENS
 
