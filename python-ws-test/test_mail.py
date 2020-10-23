f68ba9400916270199fb9335f75223a69ab32bd2
Pedro Henrique Leonardi <pedrleonardi@gmail.com>
2020-10-23 13:00:08 -0300

    Correcao 2

--- python-ws-test/test_mail.py
+++ python-ws-test/test_mail.py
@@ -9,21 +9,22 @@ SENHA_EMAIL = os.environ.get('PAS_MAIL')
 def envia_email(mensagem):
 
     print('Iniciando teste de email básico a partir do Jenkins')
-    print('Sleep para suspense')
-    time.sleep(3.5)
+    print(mensagem)
+    print('Sleep para suspense\n\n')
+    time.sleep(5.5)
 
     msg = EmailMessage()
     msg['Subject'] = 'Email com variáveis de ambiente ocultas'
     msg['From'] = ENDERECO_EMAIL
     msg['To'] = ENDERECO_EMAIL
     msg.set_content('Este email foi enviado com variáveis de ambiente ocultas e fornecidas pelo próprio jenkins\n' +
-    'Se conseguirmos ver as linguagens que estão sendo informadas aqui abaixo.\n\n', mensagem, '\n\nEntão o teste foi um sucesso :)')
+    'Se conseguirmos ver as linguagens que estão sendo informadas aqui abaixo.\n\n', str(mensagem))
 
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
         smtp.login(ENDERECO_EMAIL, SENHA_EMAIL)
         smtp.send_message(msg)
 
-    print('Email enviado com sucesso')
+    print('Então o teste foi um sucesso :)\n\n Email enviado com sucesso')
 
 if __name__ == '__main__':
     envia_email('mensagem genêrica de teste')
