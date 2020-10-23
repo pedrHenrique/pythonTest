ef936239f95b703047fee7ecd038e425c0ec0f16
Pedro Henrique Leonardi <pedrleonardi@gmail.com>
2020-10-23 12:53:30 -0300

    Correção de Valor de variável

--- python-ws-test/test_mail.py
+++ python-ws-test/test_mail.py
@@ -6,7 +6,7 @@ from email.message import EmailMessage
 ENDERECO_EMAIL = os.environ.get('USR_MAIL')
 SENHA_EMAIL = os.environ.get('PAS_MAIL')
 
-def envia_email(msg):
+def envia_email(mensagem):
 
     print('Iniciando teste de email básico a partir do Jenkins')
     print('Sleep para suspense')
@@ -17,7 +17,7 @@ def envia_email(msg):
     msg['From'] = ENDERECO_EMAIL
     msg['To'] = ENDERECO_EMAIL
     msg.set_content('Este email foi enviado com variáveis de ambiente ocultas e fornecidas pelo próprio jenkins\n' +
-    'Se conseguirmos ver as linguagens que estão sendo informadas aqui abaixo.\n\n', msg, '\n\nEntão o teste foi um sucesso :)')
+    'Se conseguirmos ver as linguagens que estão sendo informadas aqui abaixo.\n\n', mensagem, '\n\nEntão o teste foi um sucesso :)')
 
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
         smtp.login(ENDERECO_EMAIL, SENHA_EMAIL)
